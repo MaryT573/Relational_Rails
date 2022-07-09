8c4db011e0e50bea7f6ab869159927241f895ddd
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-07-09 14:22:42 -0600

    created books, book, and book index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_09_170934) do
+ActiveRecord::Schema.define(version: 2022_07_09_200944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 2022_07_09_170934) do
     t.string "name"
     t.boolean "alive"
     t.integer "number_books"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.integer "publication_date"
+    t.boolean "fiction"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
