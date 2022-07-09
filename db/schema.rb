f1dc5b344eee31deb8794f70db2fa27f2b373b4e
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-07-09 16:23:30 -0600

    create association between author and book

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_09_200944) do
+ActiveRecord::Schema.define(version: 2022_07_09_221940) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,9 +29,12 @@ ActiveRecord::Schema.define(version: 2022_07_09_200944) do
     t.boolean "fiction"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id"
+    t.index ["author_id"], name: "index_books_on_author_id"
   end
 
   create_table "songs", force: :cascade do |t|
   end
 
+  add_foreign_key "books", "authors"
 end
