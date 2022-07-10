a1c5ebe6ef3724daa00c957347623cc74b6e4052
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-07-10 15:13:43 -0600

    user story 7 and test

--- app/models/author.rb
+++ app/models/author.rb
@@ -5,6 +5,7 @@ class Author < ApplicationRecord
   has_many :books
 
   def self.ordered_by_create
-    Author.order(created_at: :asc)
+    order(created_at: :asc)
   end
+
 end
--- spec/features/authors/show_spec.rb
+++ spec/features/authors/show_spec.rb
@@ -17,4 +17,13 @@ RSpec.describe 'the authors show page' do
     expect(page).to have_content(auth.created_at)
     expect(page).to have_content(auth.updated_at)
   end
+
+  it 'displays count of books' do
+    auth = Author.create!(name: "john doe", alive: false, number_books: 10)
+    book = auth.books.create!(title: "Titled Turtles: A love story", publication_date: 2057, fiction: true)
+    book2 = auth.books.create!(title: "Why", publication_date: 1667, fiction: false)
+    visit "/authors/#{auth.id}"
+
+    expect(page).to have_content("Number of Books: 2")
+  end
 end
