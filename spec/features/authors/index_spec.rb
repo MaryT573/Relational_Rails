85f86502f67225f65e38e2f40f150e8ce95d600d
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-07-10 14:41:36 -0600

    more tests

--- spec/features/authors/index_spec.rb
+++ spec/features/authors/index_spec.rb
@@ -10,4 +10,15 @@ RSpec.describe 'the authors index page' do
     expect(page).to have_content(auth2.name)
   end
 
+  it 'sorts by most recently created' do
+    auth = Author.create!(name: "john doe", alive: false, number_books: 10)
+    auth2 = Author.create!(name: "Will Smith", alive: true, number_books: 20)
+    visit "/authors"
+    auth3 = Author.create!(name: "whoever", alive: true, number_books: 20)
+    visit "/authors"
+
+    expect(auth.name).to appear_before(auth2.name)
+    expect(auth.name).to appear_before(auth3.name)
+    expect(auth2.name).to appear_before(auth3.name)
+  end
 end
