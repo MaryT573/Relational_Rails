7db16d4dab6979c83b693049ac3b9318f76ed503
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-07-11 21:23:29 -0600

    user story 15 and tests

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,10 @@
 class Book < ApplicationRecord
   validates_presence_of :title
   validates_presence_of :publication_date
-  validates :fiction, inclusion: [true, false] 
+  validates :fiction, inclusion: [true, false]
   belongs_to :author
+
+  def self.true_bool
+    where(fiction: true)
+  end
 end
